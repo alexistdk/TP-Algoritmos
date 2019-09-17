bda4329f98c2fce4179c1df640512966f9812554
LaLElOl <54589522+LaLElOl@users.noreply.github.com>
2019-09-17 16:12:35 -0300

    Update tp.cpp

--- tp.cpp
+++ tp.cpp
@@ -3,9 +3,7 @@
 using namespace std;
 
 void presentaParticipante(char nombre[50]);
-
 void reglamento();
-
 void introduccion();
 
 struct Participante{
@@ -13,59 +11,41 @@ struct Participante{
     int turnos=5;
     int puntaje=0;
 };
-
+struct Categoria{
+    char nombreCategoria[20];
+};
 struct NodoLista{
-    
-};    
-    
-
+    Nodolista* sig;
+    Categoria info;
+};
 
 int main(){
     
-    introduccion();
-    
-    Participante participantes[5];
-    
+    introduccion();    
+    Participante participantes[5];    //Array de Structs para Participantes
     char nombreParticipante[50];
-
     cout << "Participantes, ingresen sus nombres: ";
-
     for(int i = 0; i < 5; i++){
-        
-        cin.getline(nombreParticipante,50);
-        
+        cin.getline(nombreParticipante,50);        
         strcpy(paricipantes[i].nombre,nombreParticipante);
-
         presentaParticipante(nombreParticipante);
     }
-
-    reglamento();
-    
-    
+    reglamento();    
 
     return 0;
 }
 
 void introduccion(){
-
-    cout << "Bienvenidos a ¿Quién quiere ser billonario?, el único programa que premia el conocimiento." << endl;
-    
+    cout << "Bienvenidos a ¿Quién quiere ser billonario?, el único programa que premia el conocimiento." << endl;    
     cout << "Hoy tenemos a 5 participantes nuevos que competiran por el gran premio." << endl;
-
 }
 
 void presentaParticipante(char nombre[50]){
-
-    cout << "Bienvenido " << nombre << ", le deseamos mucha suerte!" << endl;
-    
+    cout << "Bienvenido " << nombre << ", le deseamos mucha suerte!" << endl;    
 }
 
 void reglamento(){
-
-    cout << "Las reglas del juego son simples, son 7 categorias elegidas totalmente al azar. ";
-    
-    cout << "Cada participante tendrá 5 turnos y ganará el que más puntos tenga. ";
-    
+    cout << "Las reglas del juego son simples, son 7 categorias elegidas totalmente al azar. ";    
+    cout << "Cada participante tendrá 5 turnos y ganará el que más puntos tenga. ";    
     cout << "En caso de empate, se hará una pregunta a cada participante hasta que uno se equivoque." << endl;
-
 }
