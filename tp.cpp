ed39273e48d3f06d62a8c03925402719335266f2
LaLElOl <54589522+LaLElOl@users.noreply.github.com>
2019-10-17 13:15:29 -0300

    Update tp.cpp

--- tp.cpp
+++ tp.cpp
@@ -1,44 +1,50 @@
 #include <iostream>
-#include <string.h>
 using namespace std;
 
-void presentaParticipante(string nombre);
+void presentaParticipante(char nombre[50]);
 void reglamento();
 void introduccion();
 
+struct Participante{
+    char nombre[50];
+    int turnos=5;
+    int puntaje=0;
+};
+struct NodoLista{
+    
+};
+
 int main(){
     
-    string Categorias[7] = {"Historia", "Deporte", "Arte", "Geografía", "Cultura", "Entretenimiento", "Ciencia"};    
-    
-    string Participantes[5] = {"Participante1", "Participante2", "Participante3", "Participante4", "Participante5"};
-
-    string nombreParticipante;
-
-    introduccion();
-
+    introduccion();    
+    Participante participantes[5];    //Array de Structs para Participantes
+    char nombreParticipante[50];
     cout << "Participantes, ingresen sus nombres: ";
-
     for(int i = 0; i < 5; i++){
-
-        cin >> nombreParticipante;
-
-        Participantes[i] = nombreParticipante;
-
+        cin.getline(nombreParticipante,50);        
+        strcpy(paricipantes[i].nombre,nombreParticipante);
         presentaParticipante(nombreParticipante);
     }
-
-    reglamento();
+    reglamento();    
 
     return 0;
 }
 
 void introduccion(){
-
-    cout << "Bienvenidos a ¿Quién quiere ser billonario?, el único programa que premia el conocimiento." << endl;
-    
+    cout << "Bienvenidos a ¿Quién quiere ser billonario?, el único programa que premia el conocimiento." << endl;    
     cout << "Hoy tenemos a 5 participantes nuevos que competiran por el gran premio." << endl;
-
 }
+
+void presentaParticipante(char nombre[50]){
+    cout << "Bienvenido " << nombre << ", le deseamos mucha suerte!" << endl;    
+}
+
+void reglamento(){
+    cout << "Las reglas del juego son simples, son 7 categorias elegidas totalmente al azar. ";    
+    cout << "Cada participante tendrá 5 turnos y ganará el que más puntos tenga. ";    
+    cout << "En caso de empate, se hará una pregunta a cada participante hasta que uno se equivoque." << endl;
+}
+
 
 void presentaParticipante(string nombre){
 
