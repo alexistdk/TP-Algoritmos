7e60f2ae700b9c94345996ea863c36efafe10fd0
LaLElOl <54589522+LaLElOl@users.noreply.github.com>
2019-10-20 15:56:37 -0300

    Update tp.cpp

--- tp.cpp
+++ tp.cpp
@@ -1,41 +1,36 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
 
 void presentaParticipante(char nombre[]);
 void reglamento();
 void introduccion();
+int numeroAleatorio(int numMax);
 
 struct Participante{
     char nombre[50];
     int turnos = 5;
     int puntaje = 0;
 };
-struct Categoria{
+struct Categoria{                  //Cada Categoria
     Preguntas vecDatos[6];
 };
 
-struct Pregunta{
+struct Respuesta{                  //Sirve para armar el vector y hacer el multiple choice.
+    char caracter;
+    char respuesta[200];
 };
 
-struct Preguntas{
+struct Preguntas{                  //La pregunta en si, con las supuestas respuestas (1 es la correcta) y el bool para marcar el uso.
     char pregunta[500];
-    char respuesta1[200];
-    char respuesta2[200];
-    char respuesta3[200];
-    char respuesta4[200];
+    Respuesta vecRespuestas[4];
     char respuestaCorrecta;
     bool usada;
 };
 
-struct Nodo{
-    Categoria info;
-    Nodo* sig;
-};    
-
 int main(){
-    //Falta abrir todos los archivos!
-    Categoria vecCategorias[7];
-    FILE* categorias=fopen("categorias.dat","rb+");
+    FILE* categorias=fopen("categorias.dat","rb+");          //Revisar por las dudas si falta algun archivo
     FILE* participante1=fopen("participante1.dat","rb+");
     FILE* participante2=fopen("participante2.dat","rb+");
     FILE* participante3=fopen("participante3.dat","rb+");
@@ -50,8 +45,7 @@ int main(){
         cout<<"Caracter incorrecto. Ingrese s o n"<<endl;
         cin>>partidaNueva;
     }    
-    if(partidaNueva=='s'){
-       //Falta el delete de los archivos anteriores
+    if(partidaNueva=='s'){               //Cambiar en el archivo de Categorias el bool de todas las preguntas a false
        introduccion();    
        Participante participantes[5];    //Array de Structs para Participantes
        char nombreParticipante[50];
@@ -61,8 +55,8 @@ int main(){
            strcpy(paricipantes[i].nombre,nombreParticipante);
            presentaParticipante(nombreParticipante);
        }
-    reglamento();    //Falta el armado del archivo de cada participante!!!(open rb+)
-    }
+    reglamento();    
+    }                                            //Despues del if hay que armar la Cola para los turnos teniendo en cuenta a los turnos
     
     return 0;
 }
@@ -81,6 +75,13 @@ void reglamento(){
     cout << "Cada participante tendrá 5 turnos y ganará el que más puntos tenga. ";
     cout << "En caso de empate, se hará una pregunta a cada participante hasta que uno se equivoque." << endl;
 }
+
+int numeroAleatorio(int numMax){
+    int c;
+    srand(time(NULL));
+	c=1+rand()%numMax;
+	return c;
+}    
 
 /*
 funcion que inicialize el archivo con puntaje parcial e inicie con valor 0
